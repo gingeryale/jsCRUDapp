f26bd47d683df82c929cbee1a6c24bf319a60d33
ao <aolin@ymail.com>
2019-08-27 20:21:12 +0300

    first commit"

---
+++ README.md
@@ -0,0 +1,3 @@
+# Very Simple CRUD APP
+### using vanilla JS and jQuery
+### using Bootstrap for minimal styling
---
+++ contacts.js
@@ -0,0 +1,24 @@
+var contacts = [
+{
+	name:"joey friends",
+	phone:"917-213-4025",
+	email:"joey@friends.com",
+	nickname:"joey",
+	favorite: "true"
+},
+{
+	name:"phoebee friends",
+	phone:"917-213-4025",
+	email:"phoebee@friends.com",
+	nickname:"phoebee",
+	favorite: "true"
+},
+{
+	name:"ross friends",
+	phone:"917-213-4025",
+	email:"ross@friends.com",
+	nickname:"ross",
+	favorite: "false"
+}
+
+]
